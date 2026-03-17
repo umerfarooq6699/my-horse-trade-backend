61d4fcf7153f3234717a9649232681e0a9766ef4
umerfarooq6699 <umer66997@gmail.com>
2026-03-17 18:48:28 +0500

    middleware added for protection

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userCon")
+const { protect } = require("../middleware/authMiddleware")
 const multer = require("multer")
 const upload = multer()
 
 router.post("/signup", upload.none(), userController.signUpUser)
 router.post("/signin", upload.none(), userController.signInUser)
-router.get("/all-users", userController.allUsers)
+router.get("/all-users", protect, userController.allUsers)
 
 module.exports = router
